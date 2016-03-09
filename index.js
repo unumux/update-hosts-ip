ffa4a0014741bbe671c57405986919f8f29bebd3
Carl Henderson <chuckhendo@gmail.com>
2016-03-09 09:40:11 -0500

    Fixed last screwed up push

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
+var path = require("path");
 var inquirer = require("inquirer");
+var sudo = require("sudo-prompt");
 
 var getIP = require("./lib/get-ip");
 var getVMs = require("./lib/get-vms");
+
+const options = {
+    name: "Unum UX Update Hosts IP"
+};
+
 
 getVMs().then(function(choices) {
     inquirer.prompt([{
@@ -13,7 +20,10 @@ getVMs().then(function(choices) {
         choices: choices
     }], function(answers) {
         getIP(answers.vmname).then(function(ip) {
-            console.log(ip);
+            var setIPPath = path.join(__dirname, "lib", "set-ip.js");
+            sudo.exec("/usr/bin/env node " + setIPPath + " " + ip, options, function(error, stdout, stderr) {
+                console.log(error || stdout || stderr);
+            });
         });
     });
 }).catch(function(msg) {
