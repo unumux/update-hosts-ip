ea85e5382f384ed7af9d3a91296b5cc79fd779bd
Carl Henderson <chuckhendo@gmail.com>
2016-03-09 09:10:15 -0500

    Refactor code into modules

--- index.js
+++ index.js
@@ -1,44 +1,22 @@
 #!/usr/bin/env node
-var sudo = require('sudo-prompt');
-var exec = require('child_process').exec;
-var fs = require('fs');
-var path = require('path');
-var inquirer = require('inquirer');
 
-var options = {
-  name: 'Unum UX Update Hosts IP',
-};
+var inquirer = require("inquirer");
 
-exec("prlctl list --no-header -o name", function(err, stdout, stderr) {
-  var choices = stdout.split('\n').filter(function(line) { return line.trim().length > 0 });
+var getIP = require("./lib/get-ip");
+var getVMs = require("./lib/get-vms");
 
-  if(choices.length === 0) {
-      console.log("No running VMs could be found!")
-      process.exit();
-  }
-
-  inquirer.prompt([
-    {
-      type: 'list',
-      name: 'vmname',
-      message: "Choose VM",
-      choices: choices
-    }
-  ], function(answers) {
-    getIP(answers.vmname);
-  });
-});
-
-
-function getIP(vmname) {
-
-  exec('prlctl exec "' + vmname + '" ipconfig', function(error, stdout, stderr) {
-    var adapterSection = stdout.slice(stdout.indexOf("Ethernet adapter Local Area Connection:"));
-    var ip = adapterSection.match(/IPv4 Address[^:]*:(.*)/)[1].trim();
-    var setIPPath = path.join(__dirname, 'set-ip.js');
-    sudo.exec('/usr/bin/env node ' + setIPPath + ' ' + ip, options, function(error, stdout, stderr) {
-      console.log(error || stdout || stderr);
+getVMs().then(function(choices) {
+    inquirer.prompt([{
+        type: "list",
+        name: "vmname",
+        message: "Choose VM",
+        choices: choices
+    }], function(answers) {
+        getIP(answers.vmname).then(function(ip) {
+            console.log(ip);
+        });
     });
-  });
-
-}
+}).catch(function(msg) {
+    console.log(msg);
+    process.exit();
+});
