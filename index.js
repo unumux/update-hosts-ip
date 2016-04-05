6a1371802b98c63ae06d89ed9a022a95b08ec36c
Carl Henderson <chuckhendo@gmail.com>
2016-04-05 10:22:10 -0400

    update to add ability to create new hosts entry

--- index.js
+++ index.js
@@ -3,14 +3,17 @@
 var path = require("path");
 var inquirer = require("inquirer");
 var sudo = require("sudo-prompt");
+var hostile = require("hostile");
 
 var getIP = require("./lib/get-ip");
 var getVMs = require("./lib/get-vms");
+var getHosts = require("./lib/get-hosts");
 
 const options = {
     name: "Unum UX Update Hosts IP"
 };
 
+var hostsPromise = getHosts();
 
 getVMs().then(function(choices) {
     inquirer.prompt([{
@@ -18,10 +21,36 @@ getVMs().then(function(choices) {
         name: "vmname",
         message: "Choose VM",
         choices: choices
-    }], function(answers) {
+    }, {
+        type: "list",
+        name: "updateOrAdd",
+        message: "Do you want to update an existing host IP or add a new one?",
+        choices: [
+            "Update existing",
+            "Add new"
+        ]
+    }, {
+        type: "list",
+        name: "hostName",
+        message: "Which hostname should be updated?",
+        choices: function() {
+            var done = this.async();
+            hostsPromise.then(done);
+        },
+        when: function(answers) {
+            return answers.updateOrAdd === "Update existing";
+        } 
+    }, {
+        type: "input",
+        name: "hostName",
+        message: "Enter a hostname to map to VM",
+        when: function(answers) {
+            return answers.updateOrAdd === "Add new";
+        } 
+    }], function(answers) {        
         getIP(answers.vmname).then(function(ip) {
             var setIPPath = path.join(__dirname, "lib", "set-ip.js");
-            sudo.exec("/usr/bin/env node " + setIPPath + " " + ip, options, function(error, stdout, stderr) {
+            sudo.exec("/usr/bin/env node " + setIPPath + " " + ip + " \"" + answers.hostName + "\"", options, function(error, stdout, stderr) {
                 console.log(error || stdout || stderr);
             });
         });
@@ -30,3 +59,4 @@ getVMs().then(function(choices) {
     console.log(msg);
     process.exit();
 });
+
